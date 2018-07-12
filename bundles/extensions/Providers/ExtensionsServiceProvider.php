e52a46a640deaff34cb9f65cb70dcbeb1621718c
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2018-07-12 21:31:59 +0200

    register extension in service provider

--- bundles/extensions/Providers/ExtensionsServiceProvider.php
+++ bundles/extensions/Providers/ExtensionsServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Newride\Silicon\bundles\extensions\Providers;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
+use Newride\Silicon\bundles\extensions\Facades\Extensions as ExtensionsFacade;
 use Newride\Silicon\bundles\extensions\Services\Extensions;
 
 class ExtensionsServiceProvider extends ServiceProvider
@@ -19,5 +21,7 @@ class ExtensionsServiceProvider extends ServiceProvider
         foreach ($this->app->make('extensions')->all() as $extension) {
             $this->app->register($extension);
         }
+
+        AliasLoader::getInstance()->alias('Extensions', ExtensionsFacade::class);
     }
 }
