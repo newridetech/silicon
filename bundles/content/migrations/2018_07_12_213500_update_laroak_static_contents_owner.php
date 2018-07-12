36ddb9eb5016f7188038c8b15ec46f4d6f890722
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2018-07-12 21:40:48 +0200

    update database owner field

---
+++ bundles/content/migrations/2018_07_12_213500_update_laroak_static_contents_owner.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateLaroakStaticContentsOwner extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        DB::statement('UPDATE `laroak_static_contents` SET `owner_type` = replace(owner_type, \'Laroak\', \'Silicon\')');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        DB::statement('UPDATE `laroak_static_contents` SET `owner_type` = replace(owner_type, \'Silicon\', \'Laroak\')');
+    }
+}
