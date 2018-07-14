ffd840de24470050be39ecc98bada64fa6026533
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2018-07-14 18:29:30 +0200

    keycloak auth service provider -> service provider

--- bundles/keycloak/Providers/KeycloakProvider.php
+++ bundles/keycloak/Providers/KeycloakProvider.php
@@ -3,7 +3,7 @@
 namespace Newride\Silicon\bundles\keycloak\Providers;
 
 use Auth;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 use Newride\Silicon\bundles\extensions\Http\Middleware\CheckCanUseExtension as CheckCanUseExtensionMiddleware;
 use Newride\Silicon\bundles\keycloak\Auth\Guard\KeycloakSession as KeycloakSessionGuard;
 use Newride\Silicon\bundles\keycloak\Auth\Guard\KeycloakToken as KeycloakTokenGuard;
@@ -17,23 +17,35 @@ use pviojo\OAuth2\Client\Provider\Keycloak;
 class KeycloakProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        'Newride\Silicon\app\Model' => 'Newride\Silicon\app\Policies\ModelPolicy',
-    ];
-
-    /**
      * Register any authentication / authorization services.
      */
     public function boot()
     {
-        $this->registerMiddlewares();
-        $this->registerPolicies();
-        $this->registerGuards();
-        $this->registerGates();
+        $this->bootMiddlewares();
+        $this->bootGuards();
+    }
+
+    public function bootMiddlewares(): void
+    {
+        $router = $this->app->make('router');
+
+        $router->aliasMiddleware('extension', CheckCanUseExtensionMiddleware::class);
+        $router->aliasMiddleware('keycloak', CheckKeycloakRoleMiddleware::class);
+    }
+
+    public function bootGuards(): void
+    {
+        Auth::extend('keycloak.session', function ($app, $name, array $config): KeycloakSessionGuard {
+            return $app->make(KeycloakSessionGuard::class);
+        });
+
+        Auth::extend('keycloak.token', function ($app, $name, array $config): KeycloakTokenGuard {
+            return $app->make(KeycloakTokenGuard::class);
+        });
+
+        Auth::provider('keycloak', function ($app, array $config): KeycloakUserProvider {
+            return $app->make(KeycloakUserProvider::class);
+        });
     }
 
     public function register()
@@ -51,32 +63,5 @@ class KeycloakProvider extends ServiceProvider
             AuthenticationReceiverContract::class,
             AuthenticationReceiverImplementation::class
         );
-    }
-
-    public function registerMiddlewares(): void
-    {
-        $router = $this->app->make('router');
-
-        $router->aliasMiddleware('extension', CheckCanUseExtensionMiddleware::class);
-        $router->aliasMiddleware('keycloak', CheckKeycloakRoleMiddleware::class);
-    }
-
-    public function registerGates(): void
-    {
-    }
-
-    public function registerGuards(): void
-    {
-        Auth::extend('keycloak.session', function ($app, $name, array $config): KeycloakSessionGuard {
-            return $app->make(KeycloakSessionGuard::class);
-        });
-
-        Auth::extend('keycloak.token', function ($app, $name, array $config): KeycloakTokenGuard {
-            return $app->make(KeycloakTokenGuard::class);
-        });
-
-        Auth::provider('keycloak', function ($app, array $config): KeycloakUserProvider {
-            return $app->make(KeycloakUserProvider::class);
-        });
     }
 }
